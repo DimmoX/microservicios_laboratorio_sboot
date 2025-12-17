1a31696ab1282ebc957fd89bad2b51be5e4f182d
Diego Andrés Muñoz Morales <86175879+DimmoX@users.noreply.github.com>
2025-12-17 15:40:42 -0300

    feat: import file config cors

--- ms_api_gateway/src/main/java/com/api_gateway/ms_api_gateway/config/SecurityConfig.java
+++ ms_api_gateway/src/main/java/com/api_gateway/ms_api_gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.api_gateway.ms_api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -21,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
+            .cors(Customizer.withDefaults())
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
             .authorizeExchange(exchange -> exchange
                 .anyExchange().permitAll()
