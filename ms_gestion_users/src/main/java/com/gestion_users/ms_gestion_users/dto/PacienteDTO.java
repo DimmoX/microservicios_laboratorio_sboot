c64af6ceb53a69c0d2265f08414b932e9b2e0b43
Diego Andrés Muñoz Morales <86175879+DimmoX@users.noreply.github.com>
2025-12-23 14:11:58 -0300

    feat!: estrucutura de datos creación usuario tipo empleado y paciente

--- ms_gestion_users/src/main/java/com/gestion_users/ms_gestion_users/dto/PacienteDTO.java
+++ ms_gestion_users/src/main/java/com/gestion_users/ms_gestion_users/dto/PacienteDTO.java
@@ -13,5 +13,79 @@ public class PacienteDTO {
     private Long contactoId;
     private OffsetDateTime creadoEn; // solo lectura
 
-    // getters/setters …
+    // Constructores
+    public PacienteDTO() {}
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPnombre() {
+        return pnombre;
+    }
+
+    public void setPnombre(String pnombre) {
+        this.pnombre = pnombre;
+    }
+
+    public String getSnombre() {
+        return snombre;
+    }
+
+    public void setSnombre(String snombre) {
+        this.snombre = snombre;
+    }
+
+    public String getPapellido() {
+        return papellido;
+    }
+
+    public void setPapellido(String papellido) {
+        this.papellido = papellido;
+    }
+
+    public String getSapellido() {
+        return sapellido;
+    }
+
+    public void setSapellido(String sapellido) {
+        this.sapellido = sapellido;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public Long getDirId() {
+        return dirId;
+    }
+
+    public void setDirId(Long dirId) {
+        this.dirId = dirId;
+    }
+
+    public Long getContactoId() {
+        return contactoId;
+    }
+
+    public void setContactoId(Long contactoId) {
+        this.contactoId = contactoId;
+    }
+
+    public OffsetDateTime getCreadoEn() {
+        return creadoEn;
+    }
+
+    public void setCreadoEn(OffsetDateTime creadoEn) {
+        this.creadoEn = creadoEn;
+    }
 }
